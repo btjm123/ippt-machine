ada3aa15adc8b7d28242de675209b632e56ab526
Benn Tan <btjm123@Benns-MacBook-Pro.local>
2022-07-10 11:11:45 +0800

    added strict mode + situps

--- situps.py
+++ situps.py
@@ -23,7 +23,7 @@ angle2 = 0
 # Video Feed
 # Define the codec and create VideoWriter object
 out = cv2.VideoWriter('output.avi', -1, 20.0, (640,480))
-cap = cv2.VideoCapture("data/pushups.mp4")
+cap = cv2.VideoCapture(0)
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
         ret, frame = cap.read()
@@ -61,36 +61,36 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             # print(angle)
             # print(tuple(np.multiply(elbow, [1280, 720]).astype(int)))
             #Visualize
-            cv2.putText(image, str(round(angle, 3)), tuple(np.multiply(elbow, [int(cap.get(3)), int(cap.get(4))]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2, cv2.LINE_AA)
+            # cv2.putText(image, str(round(angle, 3)), tuple(np.multiply(elbow, [int(cap.get(3)), int(cap.get(4))]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2, cv2.LINE_AA)
             cv2.putText(image, str(round(angle2, 3)),
                         tuple(np.multiply(hip, [int(cap.get(3)), int(cap.get(4))]).astype(int)),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
             print(angle)
-            if angle > 170:
+            if angle2 > 100:
                 stage = "up"
-            if angle < 65 and stage == "up" and angle2 > 150:
+            if angle2 < 45 and stage == "up":
                 stage = "down"
                 counter += 1
         except:
             pass
 
         #Render curl rectangle
-        cv2.rectangle(image, (0,0), (225, 73), (245, 117, 16), -1)
+        cv2.rectangle(image, (0,0), (225, 85), (143, 224, 120), -1)
         cv2.putText(image, "REPS", (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
         cv2.putText(image, str(counter), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 1, cv2.LINE_AA)
 
-        if angle2 > 150:
-            cv2.putText(image, "BACK STRAIGHT", (60, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+        if angle2 > 45 and stage == "up":
+            cv2.putText(image, "GO HIGHER", (15, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
         else:
-            cv2.putText(image, "PLZ STRAIGHTEN BACK", (60, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1,
+            cv2.putText(image, "MAINTAIN FORM", (15, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1,
                         cv2.LINE_AA)
 
 
 
-        # Render detections
+        # Render detections rgb(248, 194, 145)
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2),
-                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                  mp_drawing.DrawingSpec(color=(38,145,229), thickness=2, circle_radius=2),
+                                  mp_drawing.DrawingSpec(color=(145, 194, 248), thickness=2, circle_radius=2)
                                   )
 
         out.write(image)
