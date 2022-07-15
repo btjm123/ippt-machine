af55700ae7466eade64b65ea12d03d32077e1dee
Osaid Mian <osaidmian220@gmail.com>
2022-07-15 16:50:28 +0400

    Adding "Cup Ears" rule to Situps
    Added functionality to detect if the wrists are close to the ears, if they arent then the reps wont count and a message would be displayed to user. There is a decent amount of tolerance to make sure there are no false negatives. Nothing else has been modified

--- situps.py
+++ situps.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import math
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
@@ -17,9 +18,29 @@ def calculate_angle(a,b,c):
         angle = 360.0 - angle
     return angle
 
+#Method to check if ear is close to the wrist
+def isEarTouching(rEar, wrist, lEar):
+    R_is_x_close = math.isclose(rEar[0],wrist[0],rel_tol=.4)
+    R_is_y_close = math.isclose(rEar[1],wrist[1],rel_tol=.4)
+    L_is_x_close = math.isclose(lEar[0],wrist[0],rel_tol=.4)
+    L_is_y_close = math.isclose(lEar[1],wrist[1],rel_tol=.4)
+    
+    #remeber to account for either right or left ear
+    if R_is_x_close == True and R_is_y_close == True:
+        print(True)
+        return True
+    elif L_is_x_close == True and L_is_y_close == True:
+        print(True)
+        return True
+    else: 
+        print(False)
+        return False
+
 stage = None
 counter = 0
 angle2 = 0
+touching = False
+earMessage = ""
 # Video Feed
 # Define the codec and create VideoWriter object
 out = cv2.VideoWriter('output.avi', -1, 20.0, (640,480))
@@ -53,7 +74,13 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                      landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
             knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
                      landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
-
+            rEar = [landmarks[8].x,
+                     landmarks[8].y]
+            lEar = [landmarks[7].x,
+                     landmarks[7].y]
+            
+            #Check if wrists are close to either ear
+            touching = isEarTouching(rEar,wrist, lEar)
 
             # Calculate angle
             angle = calculate_angle(shoulder, elbow, wrist)
@@ -68,17 +95,23 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             print(angle)
             if angle2 > 100:
                 stage = "up"
-            if angle2 < 45 and stage == "up":
+            if touching == False:
+                earMessage = "Cup your ears"
+            if touching == True:
+                earMessage = ""
+            if angle2 < 45 and stage == "up" and touching == True:
+                earMessage = ""
                 stage = "down"
                 counter += 1
+
         except:
             pass
-
         #Render curl rectangle
         cv2.rectangle(image, (0,0), (225, 85), (143, 224, 120), -1)
         cv2.putText(image, "REPS", (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
         cv2.putText(image, str(counter), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 1, cv2.LINE_AA)
-
+        #Show cup ears message if it needs to be shown 
+        cv2.putText(image, earMessage, (100, 150), cv2.FONT_HERSHEY_SIMPLEX, 2, (30, 30, 255), 1, cv2.LINE_AA)
         if angle2 > 45 and stage == "up":
             cv2.putText(image, "GO HIGHER", (15, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
         else:
